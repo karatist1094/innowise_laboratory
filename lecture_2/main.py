232af8cafbb2b65760d28cc30b0bf1e24c0e3a7e
karatist1094 <gkarat1st094@gmail.com>
2025-11-19 22:39:37 +0300

    New function generate_profile

---
+++ lecture_2/main.py
@@ -0,0 +1,10 @@
+def generate_profile(age):
+    """Determines life stage according to the age"""
+    if 0 <= age <= 12:
+        return "Child"
+    elif 13 <= age <= 19:
+        return "Teenager"
+    elif age >= 20:
+        return "Adult"
+    else:
+        return "Unknown"
