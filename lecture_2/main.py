6005605fa404e4c4730a724eb7193b56d4b3a98b
karatist1094 <gkarat1st094@gmail.com>
2025-11-19 22:40:15 +0300

    main program logic

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -8,3 +8,49 @@ def generate_profile(age):
         return "Adult"
     else:
         return "Unknown"
+
+# Greeting
+print("Hello! Please enter a little information about yourself")
+
+# Getting information
+user_name = input("Enter your full name: ")
+birth_year_str = input("Enter your year of birth: ")
+
+# Transformation and calculations
+birth_year = int(birth_year_str)
+current_age = 2025 - birth_year
+
+# Getting hobbies
+hobbies = []
+
+
+while True:
+    hobby = input("Enter a favorite hobby or type 'stop' to finish: ")
+    if hobby.lower() == "stop":
+        break
+    hobbies.append(hobby)
+
+# Profile generation
+life_stage = generate_profile(current_age)
+
+# Creating dictionary
+user_profile = {
+    "name": user_name,
+    "age": current_age,
+    "stage": life_stage,
+    "hobbies": hobbies
+}
+
+# Output
+print("PROFILE:")
+print(f"Name: {user_profile['name']}")
+print(f"Age: {user_profile['age']} years old")
+print(f"Life stage: {user_profile['stage']}")
+
+# Hobby processing
+if not user_profile['hobbies']:
+    print("You didn't mention any hobbies.")
+else:
+    print(f"Favorite Hobbies ({len(user_profile['hobbies'])}):")
+    for hobby in user_profile['hobbies']:
+        print(f"- {hobby}")
